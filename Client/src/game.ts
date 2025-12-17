0c87d3537b428901a110a0957ba36ebd967afc0b
Branko Pualić-Radujko <pualicbranko@gmail.com>
2025-12-17 19:01:15 +0100

    WD-17 - Enemy - Attacking the player

--- Client/src/game.ts
+++ Client/src/game.ts
@@ -3,14 +3,22 @@ import { Player } from './entities/player';
 import { Zombie } from './entities/zombies/zombie';
 
 export class Game {
-  private _lastTime: number = 0;
+  private _lastTime: number = 0; // in milliseconds
+
+  private _player!: Player;
+  private _zombies: Zombie[] = [];
 
   constructor(
     private canvas: Canvas,
     private ctx: CanvasRenderingContext2D,
-    private player: Player,
-    private zombie: Zombie,
-  ) {}
+  ) {
+    this.init();
+  }
+
+  init(): void {
+    this.loadPlayer();
+    this.loadZombies();
+  }
 
   start() {
     console.log('game started');
@@ -21,20 +29,37 @@ export class Game {
   }
 
   loop = (currentTime: number) => {
-    const deltaTime = (currentTime - this._lastTime) / 1000;
+    // delta time = the amount of real time that passed between two frames
+    const deltaTime = (currentTime - this._lastTime) / 1000; // both times are in miliseconds, and we want seconds
     this._lastTime = currentTime;
 
     this.canvas.clear();
     this.canvas.load();
-    this.player.update(deltaTime);
-    this.player.draw(this.ctx);
 
-    if (this.zombie.isAlive()) {
-      this.zombie.update(this.ctx, this.player, deltaTime);
-      this.zombie.draw(this.ctx);
-      // this.zombie.loseHP(deltaTime);
+    if (this._player.isAlive()) {
+      this._player.update(deltaTime);
+      this._player.draw(this.ctx);
     }
+
+    this._zombies.forEach((zombie) => {
+      if (zombie.isAlive()) {
+        zombie.update(this.ctx, this._player, deltaTime);
+        zombie.draw(this.ctx);
+      }
+    });
 
     requestAnimationFrame(this.loop);
   };
+
+  // --- PRIVATE ---
+
+  private loadPlayer(): void {
+    this._player = new Player(0, 0);
+    this._player.load(this.canvas);
+  }
+
+  private loadZombies(): void {
+    this._zombies.push(new Zombie(900, 50), new Zombie(100, 100));
+    this._zombies.forEach((zombie) => zombie.load(this.canvas));
+  }
 }
--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,6 +1,4 @@
 import { Canvas } from './canvas.js';
-import { Player } from './entities/player.js';
-import { Zombie } from './entities/zombies/zombie.js';
 import { Game } from './game.js';
 
 window.addEventListener('load', () => {
@@ -11,12 +9,6 @@ function app(): void {
   const canvas = new Canvas();
   const ctx = canvas.getContext();
 
-  const player = new Player(0, 0);
-  player.load(canvas);
-
-  const zombie = new Zombie(900, 50);
-  zombie.load(canvas);
-
-  const game = new Game(canvas, ctx, player, zombie);
+  const game = new Game(canvas, ctx);
   game.start();
 }
